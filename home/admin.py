422fa78eff843a00b389b0165cfbae60a0198954
saeedfarhang <saeedfarhang.w@gmail.com>
2020-05-03 00:27:09 +0430

    1

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from . import models
 
 # Register your models here.
+admin.site.register(models.products_category)
+admin.site.register(models.Products)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Products(models.Model):
+    persian_name = models.CharField(max_length=150)
+    english_name = models.CharField(max_length = 150)
+    ditail = models.TextField()
+    category = models.ForeignKey('products_category',on_delete=models.CASCADE,related_name='+',)
+    # brands
+    # images
+    def __str__(self):
+        return self.persian_name
+
+
+class products_category(models.Model):
+    title = models.CharField(max_length = 100)
+    def __str__(self):
+        return self.title
